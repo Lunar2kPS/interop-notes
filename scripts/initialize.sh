6c4af3306ee84b157cfcbe123e717ae6405257b9
Carlos DaLomba <carlos@2kpixelstudios.net>
2026-07-09 05:02:22 -0500

    Added auto-copy for .env

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,4 +3,6 @@ printf "Setting git user.name and user.email locally...\n"
 git config --local user.name "Carlos DaLomba"
 git config --local user.email "carlos@2kpixelstudios.net"
 
-printf "Remember to create your ./scripts/replace-names/.env file, and run the following after you're finished adding your files:\n    python ./scripts/replace-names/main.py\n"
+cp ./scripts/replace-names/.env.example ./scripts/replace-names/.env
+
+printf "Remember to update your ./scripts/replace-names/.env file, and run the following after you're finished adding your files:\n    python ./scripts/replace-names/main.py\n"
