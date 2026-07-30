39d1221f0413ae9cc80f16851b11ba49081548e1
Carlos DaLomba <carlos@2kpixelstudios.net>
2026-07-30 03:00:21 -0500

    Added on-startup.sh example with notes

---
+++ scripts/notes/on-startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Use Win + R, "shell:startup".
+# Create a new shortcut, enter in:
+#   "C:\Program Files\Git\bin\bash.exe" --login -i -c "C:/dev/scripts/on-startup.sh"
+# NOTE: Add "; exec bash" at the end if you want to keep the terminal open afterwards.
+echo "Custom Git Bash auto-startup commands starting..."
+
+autoOpenFolders=(
+    "C:/dev"
+    "C:/dev/projects/example-1"
+    "C:/dev/projects/example-2"
+)
+
+for folder in "${autoOpenFolders[@]}"; do
+    code "$folder"
+done
